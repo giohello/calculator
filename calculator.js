bf189bf5648f22fbf96cc36ad0b932a044abe358
giohello <gio2009bero@gmail.com>
2025-04-03 00:16:56 +0400

    hi

--- calculator.js
+++ calculator.js
@@ -1,11 +1,23 @@
-const express = require('express')
+const express = require('express');
 
-const app = express()
+const app = express();
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', function(req, res){
-    res.send('Hello, World!')
-})
+    res.send('Hello, World!');
+});
+
+app.get('/bmiCalculator', function(req, res){
+    res.sendFile(__dirname + '/bmiCalculator.html');
+});
+
+app.post('/bmiCalculator', function(req, res){
+    const weight = parseFloat(req.body.weight);
+    const height = parseFloat(req.body.height);
+    const bmi = weight / (height * height);
+    res.send(`Your BMI is: ${bmi}`);
+});
 
 app.listen(3000, function(){
-    console.log('Server is running on port 3000.')
-})
+    console.log('Server is running on port 3000.');
+});
